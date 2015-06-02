9fab9f2230431c45a21c88da974a8a31d71a2acc
Jordi Febrer <jordi.febrer@gmail.com>
2015-06-02 23:44:18 +0100

    Added script to check if a host is reachable

---
+++ scripts/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# removes http/https
+host="$( echo "$1" | sed -E 's#^https?://##' )"
+
+# checks if a host is reachable
+if ping -c 1 $host &> /dev/null
+then
+	echo ";-)"
+else
+	echo ":-("
+fi
